68fd7343df4f332682bff1f392e2187f2638b83b
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-05-28 19:16:11 +0100

    Here the some essential changes are made. 1/ The  array stores only a random id 2/ The local storage not store any dom element Thsi change is made because storing DOM elements in localstorage and retriev it is not the best way.

--- script.js
+++ script.js
@@ -1,147 +1,72 @@
 const container = document.getElementById("container");
+const inputTxt = document.getElementById("input");
 
-const notStartedForm = document.getElementById("not-started-form");
-const progressForm = document.getElementById("progress-form");
-const completeForm = document.getElementById("complete-form");
+let notStartedContainer = document.getElementById("not-started-container");
 
 const firstAddBtn = document.getElementById("first-add-btn");
-const secondAddBtn = document.getElementById("second-add-btn");
-const thirdAddBtn = document.getElementById("third-add-btn");
 
-// localStorage.removeItem("notStartedStored");
-// localStorage.removeItem("inProgressStored");
-// localStorage.removeItem("CompletedStored");
+const deleteBtn = document.getElementById("delete-btn");
 
-let notStartedTasks =
-  localStorage.getItem("notStartedStored") !== null
-    ? JSON.parse(localStorage.getItem("notStartedStored"))
-    : [];
+let notStartedTasksArray = localStorage.getItem("tasks")
+  ? JSON.parse(localStorage.getItem("tasks"))
+  : [];
 
-let inProgressTasks =
-  localStorage.getItem("inProgressStored") !== null
-    ? JSON.parse(localStorage.getItem("inProgressStored"))
-    : [];
+getTaskFromLocal();
 
-let completeTasks =
-  localStorage.getItem("CompletedStored") !== null
-    ? JSON.parse(localStorage.getItem("completedStored"))
-    : [];
+firstAddBtn.onclick = function () {
+  addTask();
+  // inputTxt.focus();
+};
 
-// Add new task to desired array
-function addNewTaskToArray(whichArray) {
-  const newInput = document.createElement("div");
-  newInput.classList.add("form");
-  let idForBoth = randomID();
-  newInput.setAttribute("id", `${idForBoth}`);
-
-  newInput.innerHTML = `
-  <input type="text" placeholder="New task" />
-  <button class="inside-btn edit-btn" id="edit-btn">
-    <i class="fa fa-pencil in-edit-btn"></i>
-  </button>
-  <button class="inside-btn delete-btn" id="delete-btn">
-    <i class="fa fa-trash in-delete-btn"></i>
-  </button>
-  `;
-
-  const singleTask = {
-    id: idForBoth,
-    value: newInput.outerHTML,
+// Add task to array and DOM when the add button is clicked
+function addTask() {
+  // Task data
+  const task = {
+    id: Math.floor(Math.random() * 10000),
   };
 
-  whichArray.push(singleTask);
-
-  localStorage.setItem("notStartedStored", JSON.stringify(notStartedTasks));
-  localStorage.setItem("inProgressStored", JSON.stringify(inProgressTasks));
-  localStorage.setItem("CompletedStored", JSON.stringify(completeTasks));
+  // Push to array of task
+  notStartedTasksArray.push(task);
+  // Add to DOM
+  addTaskToDOMFrom(notStartedTasksArray);
+  // Add to localStorage
+  addTaskToLocalFrom(notStartedTasksArray);
 }
 
-console.log(notStartedTasks);
-console.log(inProgressTasks);
-console.log(completeTasks);
+// display task in DOM
+function addTaskToDOMFrom(notStartedTasksArray) {
+  // First empty the container
+  notStartedContainer.innerHTML = "";
+  // Loop throgh notStartedTasksArray
+  notStartedTasksArray.forEach((task) => {
+    // Create the element
+    const newTask = document.createElement("div");
+    newTask.classList.add("form");
+    newTask.setAttribute("data-id", task.id);
 
-// Add new task to DOM
-function addNewTaskToDOM(fromWhicArray, toWhichType) {
-  toWhichType.innerHTML = "";
-  if (fromWhicArray !== null) {
-    fromWhicArray.forEach((task) => {
-      toWhichType.innerHTML += task.value;
-      // toWhichType.appendChild(task.value);
-    });
+    // Fill the element
+    newTask.innerHTML = `
+    <input type="text" placeholder="New task"/>
+    <button class="inside-btn edit-btn" id="edit-btn">
+    <i class="fa fa-pencil in-edit-btn"></i>
+    </button>
+    <button class="inside-btn delete-btn" id="delete-btn">
+    <i class="fa fa-trash in-delete-btn"></i>
+    </button>
+    `;
+
+    notStartedContainer.appendChild(newTask);
+  });
+}
+
+// Add task to localstorag
+function addTaskToLocalFrom(array) {
+  localStorage.setItem("tasks", JSON.stringify(array));
+}
+
+function getTaskFromLocal() {
+  if (localStorage.getItem("tasks")) {
+    let storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    addTaskToDOMFrom(storedTasks);
   }
-
-  localStorage.setItem("notStartedStored", JSON.stringify(notStartedTasks));
-  localStorage.setItem("inProgressStored", JSON.stringify(inProgressTasks));
-  localStorage.setItem("CompletedStored", JSON.stringify(completeTasks));
 }
-
-function updateLocalStorage(transactions) {
-  localStorage.setItem("notStartedStored", JSON.stringify(transactions));
-}
-
-function init() {
-  addNewTaskToDOM(notStartedTasks, notStartedForm);
-  addNewTaskToDOM(inProgressTasks, progressForm);
-  addNewTaskToDOM(completeTasks, completeForm);
-}
-
-init();
-
-// Generate a random ID
-function randomID() {
-  return Math.floor(Math.random() * 100);
-}
-
-// Remove a task from DOM
-function removeTask(targetedElement) {
-  targetedElement.remove();
-}
-
-// ************ Events ************** //
-
-// Add button event
-
-firstAddBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  addNewTaskToArray(notStartedTasks);
-  addNewTaskToDOM(notStartedTasks, notStartedForm);
-});
-
-secondAddBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  addNewTaskToArray(inProgressTasks);
-  addNewTaskToDOM(inProgressTasks, progressForm);
-});
-
-thirdAddBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  addNewTaskToArray(completeTasks);
-  addNewTaskToDOM(completeTasks, completeForm);
-});
-
-// Inside delete button event
-
-container.addEventListener("click", (event) => {
-  // Remove from DOM
-  let targetedEl = event.target.parentElement;
-  if (event.target.classList.contains("delete-btn")) {
-    removeTask(targetedEl);
-  }
-
-  // Remove from array
-
-  notStartedTasks = notStartedTasks.filter((item) => {
-    return item.id != targetedEl.id;
-  });
-  localStorage.setItem("notStartedStored", JSON.stringify(notStartedTasks));
-
-  inProgressTasks = inProgressTasks.filter((item) => {
-    return item.id != targetedEl.id;
-  });
-  localStorage.setItem("inProgressStored", JSON.stringify(inProgressTasks));
-
-  completeTasks = completeTasks.filter((item) => {
-    return item.id != targetedEl.id;
-  });
-  localStorage.setItem("CompletedStored", JSON.stringify(completeTasks));
-});
